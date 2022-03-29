92212a43d3a79d5b997b18bd8f8feaf6ac36f041
kbyrne <kbyrne@businessolver.com>
2022-03-29 10:41:11 -0600

    Add script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+const now = Date.now();
+const oneDay = 1000 * 60 * 60 * 24;
+const threeDays = oneDay * 3;
+const oneWeek = oneDay * 7;
+
+function renderDueDate(destinationSelector, dueDateString) {
+    let dueDate = Date.parse(dueDateString);
+    let style = '';
+    if (dueDate - now < 0) {
+        style = 'color:red';
+    } else if (dueDate - now < threeDays) {
+        style = 'color:orange';
+    } else if (dueDate - now < oneWeek) {
+        style = 'color:#ffad23';
+    } else {
+        style = 'color:green';
+    }
+    AJS.$(destinationSelector).html('<p style=' + style + '>' + dueDateString + '</p>');
+}
+
+const rows = [];
+let btn = document.getElementById("addRowButton");
+btn.addEventListener('click', event => {
+    rows.push("blah")
+    AJS.$('#rows').html(rows.map(x => '<span>' + x + '</span>'))
+});
+
+document.getElementById('fetchGoogle').addEventListener('click', event => {
+    fetch('https://raw.githubusercontent.com/kmbyrne/my-json/main/example.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log(JSON.stringify(data));
+            AJS.$('#movies').text(JSON.stringify(data));
+        })
+})
